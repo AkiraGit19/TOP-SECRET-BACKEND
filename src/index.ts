e87e6bf5e8985f1e33413b53735338aee569f05f
Akira Sanchez <akirasanchez@MacBook-Air-de-Akira.local>
2025-12-02 03:16:49 -0500

    removed /lista

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ app.use(cors({
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:3000",
-    "https://top-secret-pi.vercel.app/lista"
+    "https://top-secret-pi.vercel.app"
   ],
   credentials: true
 }));
